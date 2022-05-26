5dc37b55df8eefc5bc3a48f3bb28c809b5326b1a
Qing Li <dylan.liqing@gmail.com>
2022-05-25 21:17:36 -0700

    Update

--- data/helper.py
+++ data/helper.py
@@ -38,7 +38,7 @@ def render_img(img_paths):
         x_offset += im.size[0]
     return new_im
 
-def bar_plot(ax, data, colors=None, total_width=0.8, single_width=1, legend=True):
+def bar_plot(ax, data, colors=None, total_width=0.8, single_width=1):
     """Draws a bar plot with multiple bars per data point.
 
     Parameters
@@ -70,8 +70,6 @@ def bar_plot(ax, data, colors=None, total_width=0.8, single_width=1, legend=True
         will touch eachother within a group, values less than 1 will make
         these bars thinner.
 
-    legend: bool, optional, default: True
-        If this is set to true, a legend will be added to the axis.
     """
 
     # Check if colors where provided, otherwhise use the default color cycle
@@ -90,18 +88,10 @@ def bar_plot(ax, data, colors=None, total_width=0.8, single_width=1, legend=True
     # Iterate over all data
     for i, (name, values) in enumerate(data.items()):
         # The offset in x direction of that bar
-        x_offset = (i - n_bars / 2) * bar_width + bar_width / 2
-
-        # Draw a bar for every value of that type
-        for x, y in enumerate(values):
-            bar = ax.bar(x + x_offset, y, width=bar_width * single_width, color=colors[i % len(colors)])
-
-        # Add a handle to the last drawn bar, which we'll need for the legend
-        bars.append(bar[0])
-
-    # Draw legend if we need
-    if legend:
-        ax.legend(bars, data.keys())
+        offset = (i - n_bars / 2) * bar_width + bar_width / 2
+        x = np.arange(len(values)) + offset 
+        
+        ax.bar(x, values, width=bar_width * single_width, color=colors[i % len(colors)], label=name)
         
         
 class Iterator:
