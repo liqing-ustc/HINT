151d2727574ed716bfd5ff95675bbf4297e1ce38
Qing Li <dylan.liqing@gmail.com>
2022-05-26 11:16:54 -0700

    Update

--- transformer/layers/transformer/relative_transformer.py
+++ transformer/layers/transformer/relative_transformer.py
@@ -86,7 +86,7 @@ class RelativeTransformerDecoderLayer(torch.nn.Module):
 class RelativeTransformer(Transformer):
     def __init__(self, d_model: int = 512, nhead: int = 8, num_encoder_layers: int = 6,
                  num_decoder_layers: int = 6, dim_feedforward: int = 2048, dropout: float = 0.1,
-                 activation: ActivationFunction = F.relu):
+                 activation: ActivationFunction = F.relu, **kwargs):
 
         super().__init__(d_model, nhead, num_encoder_layers, num_decoder_layers, dim_feedforward, dropout, activation,
                          TransformerEncoderWithLayer(RelativeTransformerEncoderLayer),
